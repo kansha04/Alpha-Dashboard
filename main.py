5ae0379a2ffb38dd5566d329404ce9b40156c555
Tinovonga Mushayi <143530764+kansha04@users.noreply.github.com>
2026-01-12 17:02:17 +0100

    Added a history feature to allow user to see their previously entered stick tickers.

--- main.py
+++ main.py
@@ -9,17 +9,23 @@ def get_price(ticker):
 
 def main():
     # get user to enter a stock ticker
+    history = []
     while True:
         user_input = input("Please enter a stock ticker (eg. AAPL, MSFT): ")
-        user_input = user_input.upper()
+        user_input = user_input.upper().strip()
         if user_input == "EXIT" or user_input == "Q":
-            print("Thank you for using this stock ticker.")
+            print("Thank you for using this stock checker.")
+            print("History")
+            print("---------")
+            for ticker in history:
+                print(f"{ticker[0].ticker}: {ticker[1]:.2f}")
             break
 
         ticker = yf.Ticker(user_input)
         try:
             # Try to access ticker info to validate
             print(f"{ticker.ticker}: ${get_price(ticker):.2f}")
+            history.append([ticker, get_price(ticker)])
         except (KeyError, Exception) as e:
             print(f"Error: '{user_input}' is not a valid stock ticker or data is unavailable.\nPlease check the ticker symbol and try again.")
 
